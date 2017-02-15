586e4467bc1e185c3fa1f60bd74026c37da388b4
noam finkelstein <noam.finkelstein@gmail.com>
2017-02-15 13:11:37 -0500

    Labels

--- dwtviz.py
+++ dwtviz.py
@@ -5,26 +5,30 @@ import matplotlib.colors as col
 import matplotlib.colorbar as colbar
 from itertools import chain
 
-def dwtviz(signal, wavelet='db1', level=None, cmap_name='BuPu'):
+def dwtviz(signal, wavelet='db1', level=None, cmap_name='Blues'):
     coefs = pywt.wavedec(signal, wavelet, level=level)[1:]  # drop appox coef
     f, ax = plt.subplots(2)
-    f.subplots_adjust(hspace=0)
+    f.subplots_adjust(hspace=0.025)
 
     dwt_heatmap(coefs, ax[0], cmap_name)
+    ax[0].set_title('wavelet coefficient heatmap')
     ax[1].plot(signal)
     ax[1].set_xlim([0, len(signal)])
+    ax[1].set_title('signal', y = -0.15)
     return f
 
 def dwt_heatmap(coefs, ax, cmap_name):
-    ax.set_xticklabels([])
-    ax.set_yticklabels([])
-    chained_coefs = np.array(list(chain(*coefs)))
-    norm = col.Normalize(vmin=np.min(chained_coefs), vmax=np.max(chained_coefs))
+    ax.set_xticks([])
+
+    ax.set_yticks([(i / 9) - (1 / 18) for i in range(1, len(coefs))])
+    ax.set_yticklabels(reversed(range(len(coefs))))
+    ax.set_ylabel('levels')
+
+    norm = col.Normalize(vmin=min(chain(*coefs)), vmax=max(chain(*coefs)))
     cmap = plt.get_cmap(cmap_name)
 
     colbar_axis = colbar.make_axes(ax.figure.axes, 'right')
-    colbar.ColorbarBase(colbar_axis[0], cmap)
-    
+    colbar.ColorbarBase(colbar_axis[0], cmap, norm)
 
     height = 1 / len(coefs)
     for level, coef_level in enumerate(coefs):
