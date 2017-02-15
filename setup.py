e058cdce1ef01b4cb5b03fb054a8a4697e248461
noam finkelstein <noam.finkelstein@gmail.com>
2017-02-15 16:17:57 -0500

    Package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dwtviz',
-    version='0.0.2',
+    version='0.0.3',
     description='Creates visualizations for discrete wavelet transforms',
     url='https://github.com/n-s-f/dwtviz',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
