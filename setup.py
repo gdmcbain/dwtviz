f2f9c46aa04def3b35b3bc1e80930d6be62afe62
noam finkelstein <noam.finkelstein@gmail.com>
2017-03-20 08:49:08 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dwtviz',
-    version='0.0.4',
+    version='0.0.5',
     description='Creates visualizations for discrete wavelet transforms',
     url='https://github.com/n-s-f/dwtviz',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
